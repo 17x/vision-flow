60d172a1e2ac9327b86f9023497f8b2e66a800e1
yahone <yahone.chow@gmail.com>
2025-03-18 15:59:02 +0900

    ###

--- src/components/menu/Toolbar.tsx
+++ src/components/menu/Toolbar.tsx
@@ -2,6 +2,7 @@ import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store.ts";
 import {DynamicIcon} from 'lucide-react/dynamic';
 import {LayerDown, LayerToBottom, LayerToTop, LayerUp} from "./Icons/LayerIcons.tsx";
+import {Fragment, ReactNode} from "react";
 
 const IconSize = 20
 const IconColor = 'text-black'
@@ -12,27 +13,33 @@ const Toolbar: React.FC = () => {
     {
       Object.values(actions).map((action) => {
         const {id, icon, disabled, divide} = action;
-        let Icon = <DynamicIcon size={IconSize} name={icon}/>
+        let Icon: ReactNode
 
-        if (id === 'layerUp') {
-          Icon = <LayerUp size={IconSize} className={IconColor}/>
-        }
-        if (id === 'layerDown') {
-          Icon = <LayerDown size={IconSize} className={IconColor}/>
-        }
-        if (id === 'layerTop') {
-          Icon = <LayerToTop size={IconSize} className={IconColor}/>
-        }
-        if (id === 'layerBottom') {
-          Icon = <LayerToBottom size={IconSize} className={IconColor}/>
+        switch (id) {
+          case 'layerUp':
+            Icon = <LayerUp size={IconSize} className={IconColor}/>
+            break;
+          case 'layerDown':
+            Icon = <LayerDown size={IconSize} className={IconColor}/>
+            break;
+          case 'layerTop':
+            Icon = <LayerToTop size={IconSize} className={IconColor}/>
+            break;
+          case 'layerBottom':
+            Icon = <LayerToBottom size={IconSize} className={IconColor}/>
+            break;
+          default:
+            Icon = <DynamicIcon size={IconSize} name={icon}/>
+            break;
         }
 
-        return <>
-          <button type={'button'} key={id} disabled={disabled}
+        return <Fragment key={id}>
+          <button type={'button'} disabled={disabled}
                   className={'relative ml-1 mr-1 flex items-center cursor-pointer justify-center w-6 h-6 opacity-50   hover:bg-gray-200  hover:opacity-100 disabled:text-gray-200 disabled:cursor-default'}>
             {Icon}
           </button>
-          {divide && <div className={'w-[1px] h-4 bg-gray-400 mx-2'}></div>}</>
+          {divide && <div className={'w-[1px] h-4 bg-gray-400 mx-2'}></div>}
+        </Fragment>
       })
     }
   </div>

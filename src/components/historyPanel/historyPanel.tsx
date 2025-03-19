813d88c945124c985f2e6e3b25f1dd96b83de20c
yahone <yahone.chow@gmail.com>
2025-03-19 13:18:40 +0900

    history v1

--- src/components/historyPanel/historyPanel.tsx
+++ src/components/historyPanel/historyPanel.tsx
@@ -3,35 +3,34 @@ import {EditorContext} from "../EditorContext.tsx";
 import {useTranslation} from "react-i18next";
 import {I18nHistoryDataItem} from "../../i18n/type";
 
+let _timer = null
 
 export const HistoryPanel = () => {
   const {historyArray, historyCurrent, applyHistoryNode} = useContext(EditorContext);
   const {t} = useTranslation();
-  const parentRef = useRef<HTMLDivElement>(null);
   const targetRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log(targetRef.current, parentRef.current);
-    if (targetRef.current && parentRef.current) {
+
+    if (targetRef.current) {
       const targetElement = targetRef.current;
-      const parentElement = parentRef.current;
 
-      // Calculate the scroll position required to bring the target element into view
-      const targetOffsetTop = targetElement.offsetTop;
-      const parentScrollTop = parentElement.scrollTop;
+      const _timer = setTimeout(() => {
+        targetElement.scrollIntoView({block: "center"})
+      }, 0)
 
-      // Scroll the parent container to keep the element focused
-      parentElement.scrollTo({
-        top: targetOffsetTop - parentScrollTop, // Adjust the scroll position
-        behavior: 'smooth', // Smooth scroll animation
-      });
+      return () => {
+        if (_timer) {
+          clearTimeout(_timer)
+        }
+      }
     }
-  }, []);
+  }, [historyArray, historyCurrent]);
 
   return (
-    <div ref={parentRef} className={'h-60 overflow-x-hidden overflow-y-auto'}>
+    <div className={'h-60 overflow-x-hidden overflow-y-auto'}>
       <h1>History</h1>
-      <div className={'flex flex-col space-x-2 space-y-2 p-4 bg-gray-100'}>
+      <div className={'flex flex-col space-x-2 p-4 bg-gray-100'}>
         {
           historyArray.map((historyNode, index) => {
               const isCurr = historyNode === historyCurrent
@@ -41,7 +40,6 @@ export const HistoryPanel = () => {
                           ref={isCurr ? targetRef : null}
                           onClick={() => {
                             if (isCurr) return;
-                            console.log('current', historyNode);
                             return applyHistoryNode(historyNode);
                           }}
                           className={`flex p-1 cursor-pointer text-sm hover:bg-blue-200`}>

24e9e63c8785d0ad3555b9aa4bc2aee6a58ebb5e
yahone <yahone.chow@gmail.com>
2025-03-19 13:04:45 +0900

    history v1

--- src/components/historyPanel/historyPanel.tsx
+++ src/components/historyPanel/historyPanel.tsx
@@ -7,27 +7,38 @@ import {I18nHistoryDataItem} from "../../i18n/type";
 export const HistoryPanel = () => {
   const {historyArray, historyCurrent, applyHistoryNode} = useContext(EditorContext);
   const {t} = useTranslation();
-  const scrollEle = useRef<HTMLDivElement>(null);
+  const parentRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (scrollEle.current) {
-      // Scroll to the element after rendering
-      scrollEle.current.scrollIntoView({behavior: 'smooth', block: 'start'});
+    console.log(targetRef.current, parentRef.current);
+    if (targetRef.current && parentRef.current) {
+      const targetElement = targetRef.current;
+      const parentElement = parentRef.current;
+
+      // Calculate the scroll position required to bring the target element into view
+      const targetOffsetTop = targetElement.offsetTop;
+      const parentScrollTop = parentElement.scrollTop;
+
+      // Scroll the parent container to keep the element focused
+      parentElement.scrollTo({
+        top: targetOffsetTop - parentScrollTop, // Adjust the scroll position
+        behavior: 'smooth', // Smooth scroll animation
+      });
     }
   }, []);
 
   return (
-    <div className={'h-30 overflow-auto'}>
+    <div ref={parentRef} className={'h-60 overflow-x-hidden overflow-y-auto'}>
       <h1>History</h1>
       <div className={'flex flex-col space-x-2 space-y-2 p-4 bg-gray-100'}>
         {
           historyArray.map((historyNode, index) => {
               const isCurr = historyNode === historyCurrent
               const {label, tooltip} = t(historyNode.value.type, {returnObjects: true}) as I18nHistoryDataItem
-
               return <div key={index}
                           title={tooltip}
-                          ref={isCurr ? scrollEle : null}
+                          ref={isCurr ? targetRef : null}
                           onClick={() => {
                             if (isCurr) return;
                             console.log('current', historyNode);

f06c44250e6df7e6981577b5c6acfc7dd1bcd02c
yahone <yahone.chow@gmail.com>
2025-03-23 13:26:28 +0900

    historyPanel

--- src/components/header/toolbar/Toolbar.tsx
+++ src/components/header/toolbar/Toolbar.tsx
@@ -2,13 +2,16 @@ import {useSelector} from "react-redux";
 import {RootState} from "../../../redux/store.ts";
 import {DynamicIcon} from 'lucide-react/dynamic';
 import {LayerDown, LayerToBottom, LayerToTop, LayerUp} from "./Icons/LayerIcons.tsx";
-import {Fragment, ReactNode} from "react";
+import {Fragment, ReactNode, useContext} from "react";
+import {EditorContext} from "../../EditorContext.tsx";
+import {ActionCode} from "../../../engine/editor/editor";
 
 const IconSize = 20
 const IconColor = 'text-black'
 
 const Toolbar: React.FC = () => {
   const actions = useSelector((state: RootState) => state.toolbar.actions);
+  const {executeAction} = useContext(EditorContext)
 
   return <div className={'border-b border-gray-200 box-border'}>
     <div className={'h-10 inline-flex pl-4 items-center'}>
@@ -39,6 +42,10 @@ const Toolbar: React.FC = () => {
             <button type={'button'}
                     disabled={disabled}
                     title={id}
+                    onClick={() => {
+                      // console.log(id)
+                      executeAction(id as ActionCode)
+                    }}
                     className={'relative ml-1 rounded-sm mr-1 flex items-center cursor-pointer justify-center w-6 h-6 opacity-50   hover:bg-gray-200  hover:opacity-100 disabled:text-gray-200 disabled:cursor-default'}>
               {Icon}
             </button>

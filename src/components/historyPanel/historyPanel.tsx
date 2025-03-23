3ef48d25cffc647e876872bb939d90a5c907f675
yahone <yahone.chow@gmail.com>
2025-03-23 13:23:30 +0900

    historyPanel

--- src/components/historyPanel/historyPanel.tsx
+++ src/components/historyPanel/historyPanel.tsx
@@ -26,28 +26,29 @@ export const HistoryPanel = () => {
   }, [historyArray, historyCurrent]);
 
   return (
-    <div className={'h-60 overflow-x-hidden overflow-y-auto'}>
-      <h1>History</h1>
-      <div className={'flex flex-col space-x-2 p-4 bg-gray-100'}>
-        {
-          historyArray.map((historyNode, index) => {
-              const isCurr = historyNode === historyCurrent
-              const {label, tooltip} = t(historyNode.value.type, {returnObjects: true}) as I18nHistoryDataItem
-              return <div key={index}
-                          title={tooltip}
-                          ref={isCurr ? targetRef : null}
-                          onClick={() => {
-                            if (isCurr) return;
-                            return applyHistoryNode(historyNode);
-                          }}
-                          className={`flex p-1 cursor-pointer text-sm hover:bg-blue-200`}>
-                {isCurr && '・'}
-                <span>{label}</span>
-              </div>
-            }
-          )
-        }
+    <div className={'p-2'}>
+      <h1 className={'bg-gray-400 text-white px-2'}><span>History</span></h1>
+      <div className={'border border-gray-200 overflow-x-auto scrollbar-custom overflow-y-auto'}>
+        <div className={'flex flex-col m-2 border border-gray-200 min-h-40 bg-gray-100'}>
+          {
+            historyArray.map((historyNode, index) => {
+                const isCurr = historyNode === historyCurrent
+                const {label, tooltip} = t(historyNode.value.type, {returnObjects: true}) as I18nHistoryDataItem
 
+                return <div key={index}
+                            title={tooltip}
+                            ref={isCurr ? targetRef : null}
+                            onClick={() => {
+                              if (isCurr) return;
+                              return applyHistoryNode(historyNode);
+                            }}
+                            className={`border-b border-gray-200 px-2 py-1 cursor-pointer text-xs hover:bg-gray-400 ${isCurr ? 'bg-gray-400 text-white' : ''}`}>
+                  <span>{label}</span>
+                </div>
+              }
+            )
+          }
+        </div>
       </div>
     </div>
   );

da737525eb0f13ef0a90919c446b07ae05fc410e
yahone <yahone.chow@gmail.com>
2025-04-01 16:07:12 +0900

    ###

--- src/engine/editor/viewport/eventHandlers/wheel.ts
+++ src/engine/editor/viewport/eventHandlers/wheel.ts
@@ -5,8 +5,9 @@ function handleWheel(this: Viewport, event: WheelEvent) {
   if (event.target as HTMLElement !== this.wrapper) return
   event.preventDefault()
 
-  const {zooming, panning} = switchTouchMode(event)
-
+  const r = switchTouchMode(event)
+  const {zooming, panning, scrolling, zoomFactor, translateX, translateY} = r
+  console.log(r)
   if (zooming) {
     console.log('zooming')
   }
@@ -14,25 +15,28 @@ function handleWheel(this: Viewport, event: WheelEvent) {
   if (panning) {
     console.log('panning')
   }
-  // this.zoom(zoom)
-  // this.translateViewport(translateX, translateY)
+  this.zoom(zoomFactor)
+  this.translateViewport(translateX, translateY)
 }
 
 const switchTouchMode = (() => {
-  const THROTTLE_DISTANCE = 100
-  let didShiftByDistance = false
-  const DELAY = 500
-  let _timer: number | undefined
+  // const THROTTLE_DISTANCE = 100
+  // let didShiftByDistance = false
+  // const DELAY = 500
+  // let _timer: number | undefined
   let shiftX: number = 0
   let shiftY: number = 0
-  const ACTION_THRESHOLD = 5
+  const ACTION_THRESHOLD = 3
   const EVENT_BUFFER: WheelEvent[] = []
   let zooming = false
-  let zoom = 1
   let panning = false
+  let scrolling = false
+  let zoomFactor = 1
+  let translateX = 0
+  let translateY = 0
 
   return (event: WheelEvent) => {
-    clearTimeout(_timer)
+    // clearTimeout(_timer)
 
     shiftX += event.deltaX
     shiftY += event.deltaY
@@ -65,50 +69,54 @@ const switchTouchMode = (() => {
 
     zooming = false
     panning = false
+    scrolling = false
+    zoomFactor = 1
+    translateX = 0
+    translateY = 0
 
     if (EVENT_BUFFER.length >= ACTION_THRESHOLD) {
       const conditionOnX0 = EVENT_BUFFER.every(e => e.deltaX === -0)
       const conditionOnY1 = EVENT_BUFFER.every(e => isFloat(e.deltaY))
       const conditionOnY2 = EVENT_BUFFER.every(e => Math.abs(e.deltaY) > 4)
       const mouseScrollHorizontalFlag = EVENT_BUFFER.every(e => (Math.abs(e.deltaX) >= 40) && isUInt(e.deltaX) && e.deltaY === -0)
-      
+
       if (conditionOnX0 && conditionOnY1) {
         if (conditionOnY2) {
-          // horizontal scroll
-          panning = true
+          scrolling = true
+          translateY = -deltaY
         } else {
+          zoomFactor = 0.9
           zooming = true
         }
       } else if (mouseScrollHorizontalFlag) {
-        panning = true
+        scrolling = true
+        translateX = -deltaX
       }
     } else {
       if (isUInt(deltaX) && isUInt(deltaY)) {
         panning = true
+        translateX = -deltaX
+        translateY = -deltaY
       }
     }
 
-    /**
-     * 1. Once the touch move event has shifted by certain distance:  @didShiftByDistance
-     * it starts a Timer
-     * 2. The timer will stop if the touch move event has stopped triggering for a certain time: @delay
-     */
-
-    _timer = setTimeout(() => {
-      // console.log([...EVENT_BUFFER])
-      didShiftByDistance = false
-      shiftX = 0
-      shiftY = 0
-      EVENT_BUFFER.length = 0
-      zooming = false
-      panning = false
-    }, DELAY)
+    /* _timer = setTimeout(() => {
+       // console.log([...EVENT_BUFFER])
+       didShiftByDistance = false
+       shiftX = 0
+       shiftY = 0
+       EVENT_BUFFER.length = 0
+       zooming = false
+       panning = false
+     }, DELAY)*/
 
     return {
       zooming,
-      zoom: 0,
-      shiftX,
-      shiftY,
+      panning,
+      scrolling,
+      zoomFactor,
+      translateX,
+      translateY,
     }
   }
 })()

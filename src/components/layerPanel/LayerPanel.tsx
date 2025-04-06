ab915a462f367cbf0a3ad52e14bbdf54e4746996
yahone <yahone.chow@gmail.com>
2025-04-07 01:58:00 +0900

    layerpanel

--- src/components/layerPanel/LayerPanel.tsx
+++ src/components/layerPanel/LayerPanel.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef} from "react"
+import {useEffect, useRef, useState} from "react"
 // import {useTranslation} from "react-i18next";
 // import OptimizedDND from "./OptimizedDND.ts";
 
@@ -8,50 +8,71 @@ interface LayerPanelProps {
   handleSelectModule: (id: UID) => void;
 }
 
+const ITEM_HEIGHT = 28
 export const LayerPanel = ({data, selected, handleSelectModule}: LayerPanelProps) => {
   // const {t} = useTranslation();
   const scrollRef = useRef<HTMLDivElement>(null)
   const targetRef = useRef<HTMLDivElement>(null)
+  const [scrollTop, setScrollTop] = useState(0)
+  const [indexRange, setIndexRange] = useState([0, 10])
 
-  /*
-    const DNDRef = useRef<OptimizedDND>(null);
-    useEffect(() => {
-      if (scrollRef.current) {
-        DNDRef.current = new OptimizedDND({
-          ele: scrollRef.current,
-          data
-        })
-      }
-
-      return () => {
-        if (DNDRef.current) {
-          DNDRef.current.destroy()
-        }
-      }
-    }, [data]);*/
-  // console.log(data)
   useEffect(() => {
-  /*  targetRef.current?.scrollIntoView({
-      // behavior: "smooth",
-      // block: "start",
-      inline: "start"
-    })*/
-  }, [selected])
+    // scrollTo()
+  }, [selected, data])
+
+  const handleScroll = () => {
+    if (!scrollRef.current) return
+    const newScrollTop = scrollRef.current.scrollTop
+    const scrollUp = newScrollTop < scrollTop
+    const startIndex = Math.round(newScrollTop / ITEM_HEIGHT)
+    const endIndex = startIndex + 10
+
+    console.log(newScrollTop)
+    if (scrollUp) {
+      if (indexRange[0] >= data.length - 1) return
+    } else {
+      if (indexRange[1] >= data.length - 1) return
+
+    }
+
+    setScrollTop(newScrollTop)
+    setIndexRange([startIndex, endIndex])
+  }
+
+  const arr = []
+
+  for (let i = indexRange[0]; i < indexRange[1]; i++) {
+    const item = data[i]
+
+    if (item) {
+      arr.push(
+        <div ref={selected?.includes(item.id) ? targetRef : null}
+             style={{height: ITEM_HEIGHT}}
+             className={selected?.includes(item.id) ? 'bg-gray-400 text-white' : ''}
+             onClick={() => handleSelectModule(item.id)}
+             id={`layer-module-${item.id}`}
+             key={item.id}>
+          {/*{item.id.match(/\d+$/)[0]}*/}
+          {item.type}
+        </div>
+      )
+    }
+  }
 
   return (
     <div className={'p-2'}>
       <h1 className={'bg-gray-400 text-white px-2'}><span>Layer</span></h1>
-      <div ref={scrollRef}
-           className={'scrollbar-custom overflow-x-hidden overflow-y-auto p-2 border h-30 border-gray-200 select-none'}>
-        {
-          data?.map((item, index) => (
-            <div ref={selected?.includes(item.id) ? targetRef : null}
-                 className={selected?.includes(item.id) ? 'bg-gray-400 text-white' : ''}
-                 onClick={() => handleSelectModule(item.id)}
-                 id={`layer-module-${item.id}`}
-                 key={index}>{item.type}</div>))
-        }
+      <div className={'p-2 relative overflow-hidden'}>
+        <div ref={scrollRef}
+             onScroll={handleScroll}
+             className={'relative scrollbar-custom overflow-x-hidden overflow-y-auto p-2 border h-30 border-gray-200 select-none'}>
+          <div className={'absolute w-full top-0 left-0'} style={{
+            height: data.length * ITEM_HEIGHT,
+          }}></div>
+          <div className={'w-full sticky top-0 left-0'}>{arr}</div>
+        </div>
       </div>
     </div>
   )
 }
+

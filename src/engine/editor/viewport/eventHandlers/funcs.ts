a9306e605657bb1139fcb7f0ba4a7a5da6309d8d
yahone <yahone.chow@gmail.com>
2025-04-10 01:16:08 +0900

    ###

---
+++ src/engine/editor/viewport/eventHandlers/funcs.ts
@@ -0,0 +1,20 @@
+import Editor from '../../editor.ts'
+import Rectangle from '../../../core/modules/shapes/rectangle.ts'
+import {isInsideRotatedRect} from '../../../core/utils.ts'
+
+export function updateHoveredModules(this: Editor) {
+  const {viewport, hoveredModules} = this
+  const virtualPoint = this.getWorldPointByViewportPoint(viewport.mouseMovePoint.x, viewport.mouseMovePoint.y)
+
+  hoveredModules.clear()
+  this.getVisibleModuleMap().forEach((module) => {
+    if (module.type === 'rectangle') {
+      const {x, y, width, height, rotation} = (module as Rectangle)
+      const f = isInsideRotatedRect(virtualPoint, {x, y, width, height}, rotation)
+
+      if (f) {
+        hoveredModules.add(module.id)
+      }
+    }
+  })
+}

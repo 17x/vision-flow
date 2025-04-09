2f24db491deddfbc605b52301671f221cf249343
yahone <yahone.chow@gmail.com>
2025-04-10 00:19:46 +0900

    ###

--- src/components/header/toolbar/Toolbar.tsx
+++ src/components/header/toolbar/Toolbar.tsx
@@ -1,22 +1,20 @@
-import {useSelector} from "react-redux"
-import {RootState} from "../../../redux/store.ts"
-import {LayerDown, LayerToBottom, LayerToTop, LayerUp} from "./Icons/LayerIcons.tsx"
-import {Fragment, ReactNode, useContext} from "react"
-import {ModuleOperationType} from "../../../engine/editor/type"
-import EditorContext from "../../editorContext/EditorContext.tsx"
-import {NamedIcon} from "../../lib/icon/icon.tsx"
+import {useSelector} from 'react-redux'
+import {RootState} from '../../../redux/store.ts'
+import {LayerDown, LayerToBottom, LayerToTop, LayerUp} from './Icons/LayerIcons.tsx'
+import {Fragment, ReactNode, useContext} from 'react'
+import {ModuleOperationType} from '../../../engine/editor/type'
+import EditorContext from '../../editorContext/EditorContext.tsx'
+import {NamedIcon} from '../../lib/icon/icon.tsx'
+import {I18nFormatModule, t} from 'i18next'
+import {I18nHistoryDataItem} from '../../../i18n/type'
 
 const IconSize = 20
 const IconColor = 'text-black'
 
 const Toolbar: React.FC = () => {
   const actions = useSelector((state: RootState) => state.toolbar.actions)
-  const {executeAction,/*historyCurrent*/} = useContext(EditorContext)
+  const {executeAction/*historyCurrent*/} = useContext(EditorContext)
 
-  /*  const hasPrev = Boolean(historyCurrent.prev)
-    const hasNext = Boolean(historyCurrent.next)
-    console.log(hasPrev)
-    console.log(hasNext)*/
   return <div className={'border-b border-gray-200 box-border'}>
     <div className={'h-10 inline-flex pl-4 items-center'}>
       {
@@ -41,14 +39,19 @@ const Toolbar: React.FC = () => {
               Icon = <NamedIcon size={IconSize} iconName={icon}/>
               break
           }
+          const {tooltip} = t(id, {returnObjects: true}) as I18nHistoryDataItem
 
           return <Fragment key={id}>
             <button type={'button'}
                     disabled={disabled}
-                    title={id}
+                    title={tooltip}
                     onClick={() => {
                       // console.log(id)
-                      executeAction(id as ModuleOperationType)
+                      if (id === 'undo' || id === 'redo') {
+                        executeAction('history-' + id as ModuleOperationType)
+                      } else if (id === 'delete') {
+                        executeAction('selection-' + id as ModuleOperationType)
+                      }
                     }}
                     className={'relative ml-1 rounded-sm mr-1 flex items-center cursor-pointer justify-center w-6 h-6 opacity-50   hover:bg-gray-200  hover:opacity-100 disabled:text-gray-200 disabled:cursor-default'}>
               {Icon}

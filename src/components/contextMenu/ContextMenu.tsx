1933a48f3837f61b0b7a314894dea0901e4d1d8b
yahone <yahone.chow@gmail.com>
2025-04-16 22:54:43 +0900

    ###

--- src/components/contextMenu/ContextMenu.tsx
+++ src/components/contextMenu/ContextMenu.tsx
@@ -31,7 +31,7 @@ export const ContextMenu: FC<ContextMenuProps> = ({data: {idSet, position, copie
     return () => {
       window.removeEventListener('click', remove)
     }
-  }, [idSet, position])
+  }, [idSet, position, copiedItems])
 
   const menuItems = [
     {id: 'copy', disabled: idSet.size === 0},
@@ -53,6 +53,7 @@ export const ContextMenu: FC<ContextMenuProps> = ({data: {idSet, position, copie
       ],
     },
   ]
+
   const handleContextAction = (action: string) => {
     // console.log(action)
     switch (action) {
@@ -72,52 +73,52 @@ export const ContextMenu: FC<ContextMenuProps> = ({data: {idSet, position, copie
     // executeAction()
   }
 
-  const MenuItem = ({item}) => {
+  const MenuItem: FC<{ item: never, onClick: VoidFunction }> = ({item, onClick}) => {
     const menuText = t(item.id, {returnObjects: true}) as I18nHistoryDataItem
-    // const showDivider = index === 2 || index === 4 || index === 7
-    const showChild = item.children && item.children.length > 0
-
-    return <div
-      className={`flex justify-between min-w-30 text-nowrap w-auto items-center px-4 py-1.5 text-left text-sm hover:bg-gray-100 disabled:text-gray-400 disabled:hover:bg-transparent disabled:cursor-not-allowed`}
-      /*
-                    disabled={item.disabled}
-      */
-      onClick={() => handleContextAction(item.id)}
-      title={menuText.tooltip}
-    >
-      <span>{menuText.label}</span>
-      {showChild && <LuChevronRight/>}
+    const hasChildren = item.children && item.children.length > 0
+    // console.log(item)
+    return <div className={`flex justify-between min-w-30 text-nowrap items-center `}
+                onClick={onClick}
+                title={menuText.tooltip}>
+      <button disabled={item.disabled}
+              className={'px-4 py-1.5 block w-full text-left text-sm  hover:bg-gray-100 disabled:text-gray-400 disabled:hover:bg-transparent disabled:cursor-not-allowed'}
+      >{menuText.label}</button>
+      {hasChildren && <LuChevronRight/>}
     </div>
   }
+
   return (
     <div className={groupClass}
          style={{
            top: position.y,
            left: position.x,
          }}>
-      {menuItems.map((item, index) => {
-        // const menuText = t(item.id, {returnObjects: true}) as I18nHistoryDataItem
-        const showDivider = index === 2 || index === 4 || index === 7
-        const showChild = item.children && item.children.length > 0
+      {
+        menuItems.map((item, index) => {
+          const showDivider = index === 2 || index === 4 || index === 7
+          const showChild = item.children && item.children.length > 0
+          const menuText = t(item.id, {returnObjects: true}) as I18nHistoryDataItem
 
-        return (
-          <div key={item.id} className={'relative group'}>
+          return <div key={index} className={'relative group'}>
             {showDivider && <div className="border-t border-gray-200 my-1"></div>}
-            <MenuItem item={item}/>
+             <div onClick={()=>{
+              console.log(2000)
+            }}><MenuItem item={item}  /></div>
             {
-              showChild &&
-                <div className={groupClass + ' group-hover:block hidden left-full top-0 border-l-0 rounded-none '}>
-                  {
-                    item.children.map(child => {
-                      console.log(child)
-                      return <MenuItem item={child}/>
-                    })
-                  }
+              showChild && <div
+                    className={groupClass + ' z-60 group-hover:block hidden left-full top-0 border-l-0 rounded-none '}>
+                {
+                  item.children.map((child, childIndex) => {
+                    const childMenuText = t(child.id, {returnObjects: true}) as I18nHistoryDataItem
+
+                    return <MenuItem key={childIndex} item={child} onClick={() => handleContextAction(item.id)}/>
+                  })
+                }
                 </div>
             }
           </div>
-        )
-      })}
+        })
+      }
     </div>
   )
 }

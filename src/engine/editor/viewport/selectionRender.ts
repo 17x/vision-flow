5e12007f23e8c85fa54caf78f45366d6af5b094a
yahone <yahone.chow@gmail.com>
2025-04-16 13:24:55 +0900

    ###

--- src/engine/editor/viewport/selectionRender.ts
+++ src/engine/editor/viewport/selectionRender.ts
@@ -1,7 +1,6 @@
 import Rectangle from '../../core/modules/shapes/rectangle.ts'
 // import {drawCrossLine} from '../../lib/lib.ts'
 import Editor from '../editor.ts'
-import Ellipse, {EllipseProps} from '../../core/modules/shapes/ellipse.ts'
 
 function selectionRender(this: Editor) {
   if (this.moduleMap.size === 0) return
@@ -14,7 +13,7 @@ function selectionRender(this: Editor) {
   const lineWidth = 1 / this.viewport.scale * this.viewport.dpr
   const centerPoints = new Set<UID>(selected)
 
-  if(this.hoveredModule){
+  if (this.hoveredModule) {
     centerPoints.add(this.hoveredModule)
   }
 
@@ -36,24 +35,25 @@ function selectionRender(this: Editor) {
     const highlightModule = module!.getHighlightModule(lineWidth, fillColor) as ModuleInstance
 
     highlightModule!.render(ctx)
+    const centerDotRect = new Rectangle({
+      x,
+      y,
+      layer,
+      id: id + 'hover-center',
+      width: centerPointWidth * 2,
+      height: centerPointWidth * 2,
+      fillColor: fillColor,
+      lineColor: 'transparent',
+      lineWidth,
+      rotation,
+      opacity: 100,
+      radius: id === this.hoveredModule ? 0 : centerPointWidth,
+    })
 
+    centerDotRect.render(ctx)
     // selected
     if (id !== this.hoveredModule) {
-      const centerDotRect = new Rectangle({
-        x,
-        y,
-        layer,
-        id: id + 'hover-center',
-        width: centerPointWidth * 2,
-        height: centerPointWidth * 2,
-        fillColor: fillColor,
-        lineColor: 'transparent',
-        lineWidth,
-        rotation,
-        opacity: 100,
-      })
 
-      centerDotRect.render(ctx)
     }
   })
 
@@ -61,27 +61,27 @@ function selectionRender(this: Editor) {
     operation.module.render(ctx)
   })
 
-/*  if (this.hoveredModule) {
-    const module = this.getVisibleModuleMap.get(this.hoveredModule)
+  /*  if (this.hoveredModule) {
+      const module = this.getVisibleModuleMap.get(this.hoveredModule)
 
-    if (module) {
-      const moduleProps = module.getDetails() as EllipseProps
-      const dot = new Ellipse({
-        x: module.x,
-        y: module.y,
-        layer: module.layer,
-        r1: centerPointWidth,
-        r2: centerPointWidth,
-        fillColor,
-        lineWidth: 0,
-        lineColor: 'transparent',
-        id: moduleProps.id + 'hover-center',
-        opacity: 100,
-      })
+      if (module) {
+        const moduleProps = module.getDetails() as EllipseProps
+        const dot = new Ellipse({
+          x: module.x,
+          y: module.y,
+          layer: module.layer,
+          r1: centerPointWidth,
+          r2: centerPointWidth,
+          fillColor,
+          lineWidth: 0,
+          lineColor: 'transparent',
+          id: moduleProps.id + 'hover-center',
+          opacity: 100,
+        })
 
-      dot.render(ctx)
-    }
-  }*/
+        dot.render(ctx)
+      }
+    }*/
 
   // if (this.enableCrossLine && this.drawCrossLine) {
   /*if (this.viewport.enableCrossLine && this.viewport.drawCrossLine) {

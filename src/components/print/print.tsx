45bfe0af4589a350a80618dd66018803d4fa4043
yahone <yahone.chow@gmail.com>
2025-04-17 13:16:07 +0900

    ###

---
+++ src/components/print/print.tsx
@@ -0,0 +1,43 @@
+import {FC, memo, useEffect, useRef, useState} from 'react'
+import Editor from '../../engine/editor/editor.ts'
+
+export const Print: FC<{ onClose: VoidFunction, editorRef: { current: Editor | null } }> = memo(({
+                                                                                                   onClose,
+                                                                                                   editorRef,
+                                                                                                 }) => {
+  const printPreviewCanvas = useRef<HTMLCanvasElement>(null)
+  const [dpr, setDpr] = useState(2)
+
+  useEffect(() => {
+    createPreview()
+  }, [editorRef])
+
+  const createPreview = () => {
+    if (!editorRef || !editorRef.current || !printPreviewCanvas) {
+      // console.log(editorRef.current)
+      return
+    }
+    const {mainCanvas, frame, offset, dpr} = editorRef.current!.viewport
+    const rect = frame.getBoundingRect()
+    const sourceCanvas = mainCanvas
+    const destCanvas = printPreviewCanvas.current
+    const destCtx = destCanvas!.getContext('2d')
+
+    destCanvas!.width = rect.width * dpr
+    destCanvas!.height = rect.height * dpr
+
+    destCtx.setTransform(dpr, 0, 0, dpr, 0, 0)
+    editorRef.current.printOut(destCtx)
+    // destCtx!.drawImage(sourceCanvas, offset.x / dpr, offset.y / dpr)
+  }
+
+  return <div className={'fixed top-0 left-0 w-full h-full z-100 flex items-center justify-center'}>
+    <div className={'absolute top-0 left-0 w-full h-full bg-gray-400 opacity-70'} onClick={onClose}></div>
+    <div className={'flex w-[90%] h-[90%] bg-white rounded z-10 px-2 py-4'}>
+      <div className={'w-[50%] flex items-center justify-center overflow-hidden'}>
+        <canvas ref={printPreviewCanvas} className={'max-w-full max-h-full border'}></canvas>
+      </div>
+      <div></div>
+    </div>
+  </div>
+})

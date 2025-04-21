2934ba9ca2d190ca6206595c0755458c9423c044
yahone <yahone.chow@gmail.com>
2025-04-21 19:07:10 +0900

    0.0.2

--- src/components/CreateFile/CreateFile.tsx
+++ src/components/CreateFile/CreateFile.tsx
@@ -1,7 +1,7 @@
 import {FC, FormEvent, useContext, useRef, useState} from 'react'
 import uid from '../../utilities/Uid.ts'
 import FileContext, {FileType} from '../fileContext/FileContext.tsx'
-import {Button, Input, Modal} from '@lite-u/ui'
+import {Button, Input, Modal, Panel} from '@lite-u/ui'
 import {PAGE_PRESETS} from './pagePresets.ts'
 import {useTranslation} from 'react-i18next'
 
@@ -9,11 +9,7 @@ const CreateFile: FC<{ bg: string, onBgClick?: VoidFunction }> = ({bg = '#000000
   const formRef = useRef<HTMLFormElement>(null)
   const {t} = useTranslation()
   const {createFile, handleCreating} = useContext(FileContext)
-  const [currentPageSet, setCurrentPageSet] = useState({
-    unit: 'mm',
-    width: 210,
-    height: 297,
-  })
+  const [currentPageSet, setCurrentPageSet] = useState({...PAGE_PRESETS[0]})
   const [error, setError] = useState('')
   const validateFileName = (str: string) => {
     return /^[a-zA-Z0-9-_ ]+$/.test(str)
@@ -22,9 +18,9 @@ const CreateFile: FC<{ bg: string, onBgClick?: VoidFunction }> = ({bg = '#000000
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     e.stopPropagation()
-    const filename = formRef.current?.filename.value.trim()
-
-    if (!validateFileName(filename)) {
+    // const filename = formRef.current?.filename.value.trim()
+    // console.log(currentPageSet.name)
+    if (!validateFileName(currentPageSet.name)) {
       setError('File name can only including number space and alpha')
       return
     }
@@ -33,13 +29,16 @@ const CreateFile: FC<{ bg: string, onBgClick?: VoidFunction }> = ({bg = '#000000
     const fileId = uid()
     const newFile: FileType = {
       id: fileId,
-      name: filename,
+      name: currentPageSet.name,
       config: {
-        dpr: 2,
+        // dpr: 2,
+        // moduleIdCounter: 0,
         scale: 0,
         offset: {x: 0, y: 0},
-        moduleIdCounter: 0,
-        frame: {
+        page: {
+          ...currentPageSet,
+        },
+        /*frame: {
           id: fileId + '-frame',
           width: 1000,
           height: 1414.142857,
@@ -54,49 +53,69 @@ const CreateFile: FC<{ bg: string, onBgClick?: VoidFunction }> = ({bg = '#000000
           enableFill: true,
           x: 500,
           y: 707.0714285,
-        },
+        },*/
       },
       data: [],
     }
     createFile(newFile)
     handleCreating(false)
   }
+  console.log(currentPageSet)
 
   return <Modal backdropBg={bg} onBackdropClick={() => onBgClick && onBgClick()}>
-    <div className={'w-[90%] h-[90%] bg-white overflow-hidden flex flex-col border'}>
-      <div className={'text-center h-20 flex justify-center items-center'}>{t('file.CreateTitle')}</div>
-      <div className={'h-full flex flex-row p-4'}>
-        <div className={'bg-white p-4'}>
-          <div className={'overflow-auto flex-auto flex flex-wrap space-x-2 space-y-2'}>
-            {
-              PAGE_PRESETS.map((item, index) => {
-                return <div key={index}
-                            className={'border border-gray-200 w-30 h-30 cursor-pointer hover:border-gray-600'}
-                            onClick={() => {
+    <div className={'w-[90%] h-[90%] bg-white flex flex-col p-4'}>
+      <div className={'shadow-md rounded-sm shadow-gray-600 w-full h-full overflow-hidden  text-sm'}>
+        <Panel title={t('file.CreateTitle')}>
+          <div className={'h-full flex flex-row p-4'}>
+            <div className={'bg-white p-4'}>
+              <div className={'overflow-auto flex-auto flex flex-wrap space-x-2 space-y-2'}>
+                {
+                  PAGE_PRESETS.map((item, index) => {
+                    return <div key={index}
+                                className={'border flex text-center items-center justify-center border-gray-200 w-30 h-30 cursor-pointer hover:border-gray-600'}
+                                onClick={() => {
+                                  console.log(item)
+                                  setCurrentPageSet({
+                                    ...item,
+                                    name: 'Untitled-' + item.name,
+                                  })
+                                }}>
+                      <span>{item.name}</span>
+                    </div>
+                  })
+                }
+              </div>
+            </div>
+            <form ref={formRef}
+                  className={'w-[300px] relative flex flex-col justify-between min-h-30 z-20 p-4 bg-white rounded-xl shadow-2xl'}
+                  onSubmit={handleSubmit}>
+              <div>
+                <Input label={'File Name'} name={'filename'} placeholder="Enter file name" autoFocus
+                       value={currentPageSet.name}
+                       type={'number'}/>
+                {/* <input type="text" autoFocus id="filename" name="filename"
+                   placeholder="Enter file name"
+                   className="w-full bg-gray-100 border border-gray-300 text-gray-700 py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out"/>*/}
+                <div>
+                  <Input label={'width'} autoFocus defaultValue={currentPageSet.width} type={'number'}/>
+                  <Input label={'unit'} autoFocus defaultValue={currentPageSet.unit} type={'number'}/>
 
-                            }}>
-                  {item.name}
+                  <div>
+                    <Input label={'height'} autoFocus defaultValue={currentPageSet.height} type={'number'}/>
+                  </div>
                 </div>
-              })
-            }
-          </div>
-        </div>
-        <div className={'w-[300px] h-full flex flex-col justify-between'}>
-          <div>
-            <span>width:</span> <Input value={currentPageSet.width} type={'number'}/>
-            <span>unit:</span> <Input value={currentPageSet.unit} type={'text'}/>
-          </div>
-          <div>
-            <span>height:</span> <Input value={currentPageSet.height} type={'number'}/>
-          </div>
-          <Button type={'button'} size={'sm'}>
-            hello
-          </Button>
-          <button type="submit"
+              </div>
+              <Button type={'submit'} size={'sm'}> Create File </Button>
+              {/*<button type="submit"
                   className="mt-2 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300">
             Create File
-          </button>
-        </div>
+          </button>*/}
+            </form>
+          </div>
+        </Panel>
+        {/*<div
+          className={'text-center h-8 flex justify-center items-center bg-gray-400 text-white'}>{t('file.CreateTitle')}</div>*/}
+
       </div>
     </div>
 

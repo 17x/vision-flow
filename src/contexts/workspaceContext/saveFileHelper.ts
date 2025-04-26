7c182237af7d017505184e3377ae498b09d82594
yahone <yahone.chow@gmail.com>
2025-04-26 22:01:06 +0900

    0.0.2 - workspace

--- src/contexts/workspaceContext/saveFileHelper.ts
+++ src/contexts/workspaceContext/saveFileHelper.ts
@@ -1,43 +1,34 @@
 import JSZip from 'jszip'
 import {VisionWorkspace} from '../fileContext/FileContext.tsx'
 
-const saveFileHelper = (file, workspace: VisionWorkspace[]) => {
+const saveFileHelper = (file, workspaces: VisionWorkspace[]) => {
   const zip = new JSZip()
-  const newWorkspace = workspace.map(item => {
-    const currentWSFolder = zip.folder(item.id)
-    console.log(item.assets)
-    item.assets.forEach(asset => {
-      zip.file(asset.src, asset.imageRef)
-    })
+  const assetsFolder = zip!.folder('assets')
+
+  const newWorkspace = workspaces.map(ws => {
+    return {
+      ...ws,
+      assets: ws.assets!.map(asset => {
+        assetsFolder!.file(asset.id, asset.file)
+        return asset.id
+      }),
+    }
   })
+
   const fileJson = {
     ...file,
     workspace: newWorkspace,
   }
-  console.log(file)
-  console.log('saveFileHelper', workspace)
 
   zip.file('file.json', JSON.stringify(fileJson))
-  zip.folder('workspaces')
-  // var img = zip.folder('images')
-  /*workspace.map(item => {
-    console.log(item)
-    newWorkspace.push({
-      id: item.id,
-      name: item.name,
-      data: item.data,
-    })
-  })*/
-  // img.file('smile.gif', imgData, {base64: true})
+
   zip.generateAsync({type: 'blob'})
     .then(function (content) {
-      // see FileSaver.js
       console.log(content)
-      // saveAs(content, 'example.zip')
+
       const a = document.createElement('a')
       const url = URL.createObjectURL(content)
       a.href = url
-      // a.download = file.name + '.vz'
       a.download = file.name + '.zip'
       a.click()
       URL.revokeObjectURL(url)

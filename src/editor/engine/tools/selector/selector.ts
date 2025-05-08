e9e420948f7530c466302884d37d1199b96f14dc
yahone <yahone.chow@gmail.com>
2025-05-08 18:32:49 +0900

    0.0.2 - switch tool

---
+++ src/editor/engine/tools/selector/selector.ts
@@ -0,0 +1,15 @@
+import {Tool} from '@editor/engine/tools/tool.ts'
+
+const selection: Tool = {
+  mouseDown() {
+
+  },
+  mouseMove() {
+
+  },
+  mouseUp() {
+
+  },
+}
+
+export default selection
---
+++ src/editor/engine/tools/tool.ts
@@ -0,0 +1,5 @@
+export type Tool = {
+  mouseDown: VoidFunction
+  mouseMove: VoidFunction
+  mouseUp: VoidFunction
+}

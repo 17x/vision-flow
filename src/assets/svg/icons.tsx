da9c27d51808572065f390ba933495f28f4f26c9
yahone <yahone.chow@gmail.com>
2025-05-17 22:37:22 +0900

    ###

--- src/assets/svg/icons.tsx
+++ src/assets/svg/icons.tsx
@@ -5,7 +5,7 @@
 </svg>`*/
 
 export const lineSeg = () => <svg stroke="currentColor" fill="none" strokeWidth="1" viewBox="0 0 24 24"
-                                  strokeLinecap="round" strokeLinejoin="round" height="200px" width="200px"
+                                  strokeLinecap="round" strokeLinejoin="round" height="20" width="20"
                                   xmlns="http://www.w3.org/2000/svg">
   <path d="M6 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
   <path d="M18 6m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>

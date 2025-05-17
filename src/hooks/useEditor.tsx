0dc4e67d4d5c08209f6d7f148a5d3edf52b00e9b
yahone <yahone.chow@gmail.com>
2025-05-17 17:01:49 +0900

    ###

---
+++ src/hooks/useEditor.tsx
@@ -0,0 +1,86 @@
+// useEditorInstance.ts
+import EditorContext from './EditorContext'
+import {useContext, useEffect, useRef} from 'react'
+
+function useEditor(container: HTMLDivElement | null) {
+  const {state, dispatch, executeAction} = useContext(EditorContext)
+  const lastSavedHistoryId = useRef(0)
+  const currentHistoryId = useRef(0)
+  const needSaveLocal = useRef(false)
+  useEffect(() => {
+    if (!container) return
+
+    const editor = new Editor({
+      container: containerRef!.current,
+      elements: workspace.elements,
+      assets: workspace.assets,
+      config: {
+        dpr,
+        page,
+      },
+      events: {
+        onInitialized: () => {
+          editor.execute('switch-tool', state.currentTool)
+        },
+        onZoomed: (scale) => {
+          dispatch({type: 'SET_WORLD_SCALE', payload: scale})
+        },
+        onHistoryUpdated: (historyTree) => {
+          dispatch({type: 'SET_HISTORY_ARRAY', payload: historyTree!.toArray()})
+
+          if (historyTree.current) {
+            const newHistoryStatus = {
+              id: historyTree.current.id,
+              hasPrev: !!historyTree.current.prev,
+              hasNext: !!historyTree.current.next,
+            }
+            const newNeedSaveValue = newHistoryStatus.id !== lastSavedHistoryId.current
+            // console.log(state.historyStatus)
+
+            // console.log(state.needSave)
+            // console.log(newHistoryStatus.id, lastSavedHistoryId.current)
+            // console.log(newHistoryStatus.id !== lastSavedHistoryId.current)
+
+            currentHistoryId.current = newHistoryStatus.id
+            dispatch({type: 'SET_HISTORY_STATUS', payload: newHistoryStatus})
+            dispatch({type: 'SET_NEED_SAVE', payload: newNeedSaveValue})
+            needSaveLocal.current = newNeedSaveValue
+          }
+        },
+        onModulesUpdated: (moduleMap) => {
+          const arr = Array.from(moduleMap.values()).sort((a, b) => a.layer - b.layer)
+
+          setSortedModules(arr)
+        },
+        onSelectionUpdated: (selected, props) => {
+          dispatch({type: 'SET_SELECTED_MODULES', payload: Array.from(selected)})
+          dispatch({type: 'SET_SELECTED_PROPS', payload: props})
+        },
+        onViewportUpdated: (viewportInfo) => {
+          dispatch({type: 'SET_VIEWPORT', payload: viewportInfo})
+        },
+        onWorldMouseMove: (point) => {
+          // dispatch({type: 'SET_WORLD_POINT', payload: point})
+          // worldPoint.current = point
+          if (worldPointRef.current) {
+            worldPointRef.current.set(point)
+          }
+        },
+        onContextMenu: (position) => {
+          setShowContextMenu(true)
+          setContextMenuPosition(position)
+        },
+        onModuleCopied: (items) => {
+          dispatch({type: 'SET_COPIED_ITEMS', payload: items})
+        },
+        onSwitchTool: (toolName) => {
+          dispatch({type: 'SET_CURRENT_TOOL', payload: toolName})
+        },
+      },
+    })
+
+    return () => editor.destroy()
+  }, [container])
+}
+
+export default useEditor
---
+++ src/hooks/useFocus.tsx
@@ -0,0 +1,33 @@
+import {useCallback, useEffect} from 'react'
+
+const useFocus = (element: HTMLElement | null) => {
+
+  const checkInside = useCallback((e: MouseEvent) => {
+    if (element) {
+      dispatch({type: 'SET_FOCUSED', payload: element.contains(e.target as Node)})
+    }
+  }, [])
+
+  const handleFocus = () => {
+    dispatch({type: 'SET_FOCUSED', payload: true})
+  }
+
+  const handleBlur = () => {
+    dispatch({type: 'SET_FOCUSED', payload: false})
+  }
+  useEffect(() => {
+    if (!element) return
+
+    window.addEventListener('mouseup', checkInside)
+    element.addEventListener('focus', handleFocus)
+    element.addEventListener('blur', handleBlur)
+
+    return () => {
+      window.removeEventListener('mouseup', checkInside)
+      element.removeEventListener('focus', handleFocus)
+      element.removeEventListener('blur', handleBlur)
+    }
+  }, [element])
+
+}
+export default useFocus

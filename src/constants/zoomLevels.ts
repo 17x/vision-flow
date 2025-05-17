c7418fb666ef279deb2888f57a1fb88905ab928d
yahone <yahone.chow@gmail.com>
2025-05-17 12:23:50 +0900

    ###

---
+++ src/constants/zoomLevels.ts
@@ -0,0 +1,21 @@
+const ZOOM_LEVELS: { label: string, value: number | 'fit' }[] = [
+  {label: '64000%', value: 640},
+  {label: '3200%', value: 32},
+  {label: '1600%', value: 16},
+  {label: '800%', value: 8},
+  {label: '400%', value: 4},
+  {label: '200%', value: 2},
+  {label: '150%', value: 1.5},
+  {label: '100%', value: 1},
+  {label: '66.67%', value: 0.6667},
+  {label: '50%', value: 0.5},
+  {label: '33.33%', value: 0.3333},
+  {label: '25%', value: 0.25},
+  {label: '12.5%', value: 0.125},
+  {label: '6.25%', value: 0.0625},
+  {label: '3.13%', value: 0.0313},
+  {label: '1.56%', value: 0.0156},
+  {label: 'Fit window', value: 'fit'},
+]
+
+export default ZOOM_LEVELS

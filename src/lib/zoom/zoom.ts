15caaf2a7ce3d6892772aafa5d4d5a03dbafc0ac
yahone <yahone.chow@gmail.com>
2025-05-17 10:03:49 +0900

    ###

--- src/lib/zoom/zoom.ts
+++ src/lib/zoom/zoom.ts
@@ -1,3 +1,9 @@
+export interface ZoomOptions {
+  dom: HTMLElement
+  mouse: true,
+  touchpad: true,
+}
+
 class Zoom {
   dom: HTMLElement
   mouse: boolean
@@ -7,8 +13,7 @@ class Zoom {
                 dom,
                 mouse = true,
                 touchpad = true,
-              }) {
-
+              }: ZoomOptions) {
     this.dom = dom
     this.mouse = mouse
     this.touchpad = touchpad

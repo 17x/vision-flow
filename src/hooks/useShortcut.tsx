3cbe2d9ce75c6a8bd8a22f4e341b9a3b855e90e6
yahone <yahone.chow@gmail.com>
2025-05-18 15:05:23 +0900

    ###

--- src/hooks/useShortcut.tsx
+++ src/hooks/useShortcut.tsx
@@ -3,6 +3,7 @@ import WorkspaceContext from '../contexts/workspaceContext/WorkspaceContext.tsx'
 import {EditorExecutor} from '../components/workspace/Workspace.tsx'
 import SHORTCUTS_DATA from '../constants/actions.ts'
 import Shortcut from '../lib/shortcut/shortcut.ts'
+import matchObject from '../utilities/find.ts'
 
 const useShortcut = (executeAction: EditorExecutor) => {
   const {state: {focused}} = useContext(WorkspaceContext)
@@ -11,10 +12,14 @@ const useShortcut = (executeAction: EditorExecutor) => {
   useEffect(() => {
     if (!pluginRef.current) {
       const data = []
-
-      SHORTCUTS_DATA.forEach((data) => {
-        console.log(data)
+      const a = matchObject(SHORTCUTS_DATA, (item) => {
+        return item.shortcut
       })
+
+      console.log(a)
+      /*SHORTCUTS_DATA.forEach((data) => {
+        // console.log(data)
+      })*/
       pluginRef.current = new Shortcut({
         shortcuts: data,
         callback: (code) => {

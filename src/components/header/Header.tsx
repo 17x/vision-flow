f6cc75274e9f0c40fdb075ec2b5e4ee2b21e46dc
yahone <yahone.chow@gmail.com>
2025-05-18 16:59:04 +0900

    ###

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,6 @@ import MenuBar from './menu/Menu.tsx'
 import ShortcutBar from './shortcutBar/ShortcutBar.tsx'
 
 const Header: React.FC = () => {
-  console.log(9)
   return <header>
     <MenuBar/>
     <ShortcutBar/>

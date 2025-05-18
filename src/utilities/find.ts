8bf9369420a3f4dbd77b39f38dd99d9f58cb199e
yahone <yahone.chow@gmail.com>
2025-05-18 15:11:26 +0900

    ###

---
+++ src/utilities/find.ts
@@ -0,0 +1,37 @@
+import typeCheck from './typeCheck.ts'
+
+const matchObject = (o: unknown[] | { children?: unknown[] }, predictor: (...args) => boolean) => {
+  const result: unknown[] = []
+
+  if (Array.isArray(o)) {
+    o.forEach(item => {
+      console.log(item)
+      if (predictor(item)) {
+        result.push(item)
+      }
+
+      if (item.children) {
+        item.children.forEach(child => {
+          console.log(matchObject(child, predictor))
+          result.push(...matchObject(child, predictor))
+        })
+      }
+    })
+  } else if (typeCheck(o) === 'object') {
+    if (predictor(o)) {
+      result.push(o)
+    }
+
+    if (o.children) {
+      o.children.forEach(child => {
+        // console.log(matchObject(child, predictor))
+        result.push(...matchObject(child, predictor))
+      })
+    }
+
+  }
+
+  return result
+}
+
+export default matchObject

4138cbf0f5d3f2e2810bc2114c7486b55c0f20fd
yahone <yahone.chow@gmail.com>
2025-06-23 16:09:32 +0900

    ###

--- src/components/language/languageSwitcher.tsx
+++ src/components/language/languageSwitcher.tsx
@@ -2,7 +2,7 @@ import {useTranslation} from 'react-i18next'
 import {useState} from 'react'
 import Select from '@lite-u/ui/select'
 import SelectItem from '@lite-u/ui/selectItem'
-import {Row} from '@lite-u/ui'
+import {Con, Row} from '@lite-u/ui'
 
 type LanguageCode = 'en' | 'cn' | 'jp'
 
@@ -17,17 +17,22 @@ const LanguageSwitcher: React.FC = () => {
   const {i18n} = useTranslation()
 
   return <Row center je mr={30} className="text-sm select-none">
-    <span>🌐</span>
-    <Select style={{border:'none'}} selectValue={language} onSelectChange={(langCode) => {
-      i18n.changeLanguage(langCode as LanguageCode)
-      setLanguage(langCode as LanguageCode)
-    }}>
-      {
-        (Object.keys(languageRecord) as LanguageCode[]).map((langCode, index) => {
-          return <SelectItem key={index} value={langCode}>{languageRecord[langCode]}</SelectItem>
-        })
-      }
-    </Select>
+    <Con rela w={100}>
+      <Row fw fh abs center t={0} l={0} bg={'white'} style={{zIndex: 100, pointerEvents: 'none'}}>
+        <span>🌐</span>
+        <span>{languageRecord[language]}</span>
+      </Row>
+      <Select style={{border: 'none'}} selectValue={language} onSelectChange={(langCode) => {
+        i18n.changeLanguage(langCode as LanguageCode)
+        setLanguage(langCode as LanguageCode)
+      }}>
+        {
+          (Object.keys(languageRecord) as LanguageCode[]).map((langCode, index) => {
+            return <SelectItem key={index} value={langCode}>{languageRecord[langCode]}</SelectItem>
+          })
+        }
+      </Select>
+    </Con>
   </Row>
 }
 
